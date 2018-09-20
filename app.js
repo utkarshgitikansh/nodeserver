8bd051f3741f5ba517bd4ae682b4ea462d47be1f
unknown <utkarshgitikansh@gmail.com>
2018-09-20 13:35:09 +0530

    10th commit

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ i = 0;
 
 global.data = "Hi";
 // fs.writeFile('type.txt',data);
-const PORT1 = process.env.PORT || 8080;
-app.listen(PORT1, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(process.env.PORT, () => {
   
     http.get('http://cricapi.com/api/matches/YQcxw12HpBMe1UaJ6TsKtZTC3Br2', (res) => {
     
@@ -73,12 +73,108 @@ app.listen(PORT1, () => {
 
 });
 
-app.get('/', (req, res) => {
+// app.get('/api', (req, res) => {
           
-  res.send(data);
+//   res.send(data);
 
-  }).on('error', (e) => {
+//   }).on('error', (e) => {
       
+//     console.error(`Got error: ${e.message}`);
+//   });
+ 
+
+app.get('/api', (req, res) => {
+
+    res.send(data);
+
+ // app.listen(8080, () => {
+  
+    // http.get('http://cricapi.com/api/matches/YQcxw12HpBMe1UaJ6TsKtZTC3Br2', (res) => {
+    
+  
+    // const { statusCode } = res;
+    // const contentType = res.headers['content-type'];
+   
+    // let error;
+    // if (statusCode !== 200) {
+    //   error = new Error('Request Failed.\n' +
+    //                     `Status Code: ${statusCode}`);
+    // } else if (!/^application\/json/.test(contentType)) {
+    //   error = new Error('Invalid content-type.\n' +
+    //                     `Expected application/json but received ${contentType}`);
+    // }
+    // if (error) {
+    //   console.error(error.message);
+  
+    //   res.resume();
+    //   return;
+    // }
+  
+    // //res.setEncoding('utf8');
+    // let rawData = '';
+    // res.on('data', (chunk) => { rawData += chunk; });
+    // res.on('end', () => {
+    //   try {
+    //     const parsedData = JSON.parse(rawData);
+    //     // data = parsedData;
+    //     // console.log(parsedData);
+    //     res1.send(parsedData);
+        
+    //   for(var i = 0; i < parsedData.matches.length;i++){
+    //       map.push(parsedData.matches[i]["unique_id"]);
+    //         i++;
+    // }
+  
+    //   m =  map.reduce((min, p) => p < min ? p : min, map[0]);
+   
+    //   value = m;
+    //   // console.log(map);
+    //   // map.forEach( (value) => {
+    //   // })   
+  
+  
+  
+    //   } catch (e) {
+    //     console.error(e.message);
+    //   }
+  
+  
+   
+  
+  //  });
+  }).on('error', (e) => {
+  
     console.error(`Got error: ${e.message}`);
   });
+
+//});
+
+//   var getAccessToken = 'http://cricapi.com/api/matches/YQcxw12HpBMe1UaJ6TsKtZTC3Br2';
+  
+     
+//    request(getAccessToken, function (error, response, body) {
+//      console.log(body);
+//        res.json(JSON.parse(body));
+  
+
+//  }).on('error', (e) => {
+     
+//    console.error(`Got error: ${e.message}`);
+//  });
+
+
+//})
+
+
+app.get('/timestamp', (req, res) => {
+    res.send(data);
  
+})
+
+app.get('/timestamp-cached', (req, res) => {
+    res.set('Cache-Control','public,max-age=300,s-maxage=600');
+    res.send(`${Date.now()}`);
+})
+
+
+ exports.app = functions.https.onRequest(app);
